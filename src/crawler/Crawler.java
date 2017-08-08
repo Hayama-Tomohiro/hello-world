c53213135ed42b0b2cef2149fdaadda4e17825b3
MEDIADO\tomohiro_hayama <tomohiro_hayama@H-00552.mediado.local>
2017-08-08 14:17:58 +0900

    Apache POIを使用して、Excelからデータの読み込み、出力を学習しました。

--- src/crawler/Crawler.java
+++ src/crawler/Crawler.java
@@ -1,7 +1,15 @@
 package crawler;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.text.NumberFormat;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -9,6 +17,13 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -48,7 +63,9 @@ public class Crawler {
         Gson gson = new Gson();
 
         // 取得したいJSON
-        String json = "http://zozo.jp/coupon/json/couponitemjson_10321_all.txt?_=1501808125476";
+        String json = "http://zozo.jp/coupon/json/couponitemjson_10316_all.txt?_=1502094095948";
+        // 変換後オブジェクト
+        ItemWrapper itemWrapper = null;
 
         // HttpClientを、HttpClientBuilderで作成するか、簡易にHttpClientsから取得。戻り値の型は、CloseableHttpClient
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
@@ -61,7 +78,7 @@ public class Crawler {
                     // レスポンスのBODYを取得
                     HttpEntity entity = response.getEntity();
                     // JSONからの変換
-                    ItemWrapper itemWrapper = gson.fromJson(EntityUtils.toString(entity, StandardCharsets.UTF_8), ItemWrapper.class);
+                    itemWrapper = gson.fromJson(EntityUtils.toString(entity, StandardCharsets.UTF_8), ItemWrapper.class);
                     for (ListItem item : itemWrapper.getItem().getData().getItemlist()) {
                         System.out.println(item.getGoodsid() + "は、" + item.getPrice() + "円です。");
                     }
@@ -70,6 +87,139 @@ public class Crawler {
             catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        /*
+        Workbook wb = new XSSFWorkbook();
+        Sheet sheet1 = wb.createSheet("Sheet1");
+
+        try (OutputStream out = new FileOutputStream("doc/sample.xlsx")) {
+            wb.write(out);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
+
+        try (InputStream in = new FileInputStream("doc/sample.xlsx");
+                Workbook wb = WorkbookFactory.create(in);
+                OutputStream out = new FileOutputStream("doc/sample.xlsx")) {
+            Sheet sheet = wb.getSheetAt(0);
+            Row row = sheet.createRow(0);
+            Cell cell = row.createCell(0);
+            cell.setCellValue("〇");
+            wb.write(out);
+            System.out.println(cell.getStringCellValue());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Excel読み込み
+        try (InputStream in = new FileInputStream("doc/input.xlsx");
+                Workbook wbIn = WorkbookFactory.create(in)) {
+
+            // 出力用Workbookを作成
+            Workbook wbOut = new XSSFWorkbook();
+            Sheet sheetOut = wbOut.createSheet("チェック結果");
+            // ヘッダー行を作成
+            Row rowOut0 = sheetOut.createRow(0);
+            rowOut0.createCell(0).setCellValue("商品ID");
+            rowOut0.createCell(1).setCellValue("価格");
+            rowOut0.createCell(2).setCellValue("サイト価格");
+            rowOut0.createCell(3).setCellValue("チェック結果");
+
+            // 入力用Workbookの一番最初のシートを選択
+            Sheet sheetIn = wbIn.getSheetAt(0);
+
+            // 商品ID、価格を読み取り、Mapに格納
+            Map<String, Integer> map = new LinkedHashMap<>();
+
+            for (int i = 0; i <= sheetIn.getLastRowNum(); i++) {
+                // ヘッダー行の場合
+                if (i == 0) {
+                    Row header = sheetIn.getRow(i);
+                    // 正しいヘッダーになっているかチェック
+                    if (!StringUtils.equals(header.getCell(0).getStringCellValue(), "商品ID")) {
+                        throw new Exception();
+                    }
+                    if (!StringUtils.equals(header.getCell(1).getStringCellValue(), "価格")) {
+                        throw new Exception();
+                    }
+                    continue;
+                }
+                String key = "";
+                int value = 0;
+                Row row = sheetIn.getRow(i);
+                for (int j = 0; j < row.getLastCellNum(); j++) {
+                    // 商品ID列の場合
+                    if (j == 0) {
+                        if (row.getCell(j).getCellTypeEnum() == CellType.STRING) {
+                            key = row.getCell(j).getStringCellValue();
+                        }
+                        else if (row.getCell(j).getCellTypeEnum() == CellType.NUMERIC) {
+                            key = Integer.toString((int) row.getCell(j).getNumericCellValue());
+                        }
+                        else {
+                            throw new Exception();
+                        }
+                    }
+                    // 価格列の場合
+                    else if (j == 1) {
+                        if (row.getCell(j).getCellTypeEnum() == CellType.STRING) {
+                            value = Integer.parseInt(row.getCell(j).getStringCellValue());
+                        }
+                        else if (row.getCell(j).getCellTypeEnum() == CellType.NUMERIC) {
+                            value = (int) row.getCell(j).getNumericCellValue();
+                        }
+                    }
+                    // 3列目がある場合
+                    else {
+                        throw new Exception();
+                    }
+                }
+                map.put(key, value);
+            }
+
+            // ファイルから読み込んだMapをもとにサイト価格と突き合わせ、結果出力
+            int rownum = 1; // ← 0はヘッダー行
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                // 明細行を作成
+                Row rowOut = sheetOut.createRow(rownum++);
+                rowOut.createCell(0).setCellValue(entry.getKey());
+                rowOut.createCell(1).setCellValue(entry.getValue());
+                // 価格チェック
+                for (ListItem item : itemWrapper.getItem().getData().getItemlist()) {
+                    if (StringUtils.equals(entry.getKey(), item.getGoodsid())) {
+                        rowOut.createCell(2).setCellValue(NumberFormat.getInstance().parse(item.getPrice()).intValue());
+                        if (entry.getValue() == NumberFormat.getInstance().parse(item.getPrice()).intValue()) {
+                            rowOut.createCell(3).setCellValue("〇");
+                        }
+                        else {
+                            rowOut.createCell(3).setCellValue("×");
+                        }
+                        break;
+                    }
+                }
+                if (rowOut.getCell(2) == null) {
+                    rowOut.createCell(2).setCellValue("存在しません");
+                    rowOut.createCell(3).setCellValue("×");
+                }
+            }
+
+            map.forEach((key, value) -> System.out.println(key + ":" + value));
+
+            // 結果を出力
+            try (OutputStream out = new FileOutputStream("doc/output.xlsx")) {
+                wbOut.write(out);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
         catch (Exception e) {
             e.printStackTrace();
